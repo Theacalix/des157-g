1f39f8a8ffe45bc9778e59371a3789365013f021
Theacalix <Theacalix@users.noreply.github.com>
2019-02-05 21:47:34 -0800

    added project research, added changing image drag

--- studio2/script.js
+++ studio2/script.js
@@ -1,59 +1,66 @@
 var dragged;
-var images = ['']
+var images = ['charger.svg', 'headphones.svg', 'keys.svg', 'laptop.svg', 'lunch.svg', 'mouse.svg', 'notebook.svg', 'pencilcase.svg', 'phone.svg', 'sketchbook.svg', 'umbrella.svg', 'waterbottle.svg'];
+var newImg;
 
-  /* events fired on the draggable target */
-  /*document.addEventListener("drag", function( event ) {
+function getImg() {
+  num =  Math.floor(Math.random() * images.length);
+  console.log('num: ' + num);
+  newImg = images.splice(num, 1)[0];
+  console.log('setting new image to: ' + newImg);
+  console.log('images length: ' + images.length);
+}
 
-  }, false);*/
+document.addEventListener("dragstart", function(event) {
+  var img = new Image();
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  event.dataTransfer.setData('text/plain', null);
+  getImg();
+  img.src = 'images/' + newImg;
+  event.dataTransfer.setDragImage(img, 10, 10);
+}, false);
 
-  document.addEventListener("dragstart", function( event ) {
-      // store a ref. on the dragged elem
-      dragged = event.target;
-      // make it half transparent
-      //event.target.style.opacity = .5;
-      event.dataTransfer.setData('text/plain',null);
-      //event.dataTransfer.dropEffect = 'copy';
-  }, false);
+document.addEventListener("dragend", function(event) {
+  // reset the transparency
+  //event.target.style.opacity = "";
+  if(images.length == 0) {
+    dragged.draggable = 'false';
+  }
+}, false);
 
-  document.addEventListener("dragend", function( event ) {
-      // reset the transparency
-      event.target.style.opacity = "";
-  }, false);
+/* events fired on the drop targets */
+document.addEventListener("dragover", function(event) {
+  // prevent default to allow drop
+  event.preventDefault();
+}, false);
 
-  /* events fired on the drop targets */
-  document.addEventListener("dragover", function( event ) {
-      // prevent default to allow drop
-      event.preventDefault();
-      //event.dataTransfer.dropEffect = 'copy';
-  }, false);
+document.addEventListener("dragenter", function(event) {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "#E6E7E8";
+  }
 
-  document.addEventListener("dragenter", function( event ) {
-      // highlight potential drop target when the draggable element enters it
-      if ( event.target.className == "dropzone" ) {
-          event.target.style.background = "#E6E7E8";
-      }
+}, false);
 
-  }, false);
+document.addEventListener("dragleave", function(event) {
+  // reset background of potential drop target when the draggable element leaves it
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+  }
 
-  document.addEventListener("dragleave", function( event ) {
-      // reset background of potential drop target when the draggable element leaves it
-      if ( event.target.className == "dropzone" ) {
-          event.target.style.background = "";
-      }
+}, false);
 
-  }, false);
+document.addEventListener("drop", function(event) {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged elem to the selected drop target
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+    //dragged.parentNode.removeChild( dragged );
+    var nodeCopy = dragged.cloneNode(true);
+    nodeCopy.id = '';
+    nodeCopy.src = 'images/' + newImg;
+    event.target.appendChild(nodeCopy);
+  }
 
-  document.addEventListener("drop", function( event ) {
-      // prevent default action (open as link for some elements)
-      event.preventDefault();
-      // move dragged elem to the selected drop target
-      if ( event.target.className == "dropzone" ) {
-          event.target.style.background = "";
-          //dragged.parentNode.removeChild( dragged );
-          var nodeCopy = dragged.cloneNode(true);
-          nodeCopy.id = "newId";
-          event.target.appendChild(nodeCopy);
-
-      }
-
-  }, false);
+}, false);
