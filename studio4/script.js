81966cc2baa2693d75fbb40a42915599af988ba9
Theacalix <Theacalix@users.noreply.github.com>
2019-03-04 10:00:11 -0800

    added pan

--- studio4/script.js
+++ studio4/script.js
@@ -1,6 +1,6 @@
 // import { Droppable }  from 'https://cdn.jsdelivr.net/npm/@shopify/draggable@1.0.0-beta.8/lib/draggable.bundle.min.js';
-// 'use strict';
-// console.log('reading js');
+'use strict';
+console.log('reading js');
 //
 // const droppable = new Droppable(document.querySelectorAll('section .inner'), {
 //   draggable: '.tile',
@@ -10,15 +10,27 @@
 //   }
 // });
 
-//ACCORDION MENU
 var acc = document.querySelectorAll('.accordion');
+var but = document.querySelectorAll('.mode button');
+var inner = document.querySelector('.inner');
+var viewRect = document.querySelector('.view').getBoundingClientRect();
+var boxRect = document.querySelector('.box').getBoundingClientRect();
+var cols = inner.childElementCount * boxRect.width;
+var rows = document.querySelector('.row').childElementCount * boxRect.height;
+var initialX, initialY;
+var offsetX = 0,
+  offsetY = 0;
 
-for(var i = 0; i < acc.length; i++) {
+console.log('cols: ' + cols);
+console.log('rows: ' + rows);
+
+//ACCORDION MENU
+for (var i = 0; i < acc.length; i++) {
   acc[i].addEventListener('click', function() {
     var icon = this.firstChild;
     var panel = this.nextElementSibling;
     // console.log(icon);
-    if( panel.style.display === 'flex') {
+    if (panel.style.display === 'flex') {
       panel.style.display = 'none';
       icon.className = 'fas fa-caret-right';
       this.style.borderBottom = '2px solid #cedad9';
@@ -31,10 +43,54 @@ for(var i = 0; i < acc.length; i++) {
   })
 }
 //MODE BUTTONS
-var but = document.querySelectorAll('.mode button');
+but[0].addEventListener('click', function() { //mouse -drag
+  if (this.id != 'active') {
+    this.id = 'active';
+    but[1].id = '';
+    inner.removeEventListener('mousedown', startPan);
+    inner.removeEventListener('mouseup', endPan);
+  }
+});
 
-for(var i = 0; i < but.length; i++) {
-  but[i].addEventListener('click', function() {
-    
-  })
+but[1].addEventListener('click', function() { //pan
+  if (this.id != 'active') {
+    this.id = 'active';
+    but[0].id = '';
+    inner.addEventListener('mousedown', startPan);
+    inner.addEventListener('mouseup', endPan);
+  }
+});
+
+function startPan(event) {
+  console.log('startPan');
+  initialX = event.clientX;
+  initialY = event.clientY;
+  offsetX = parseInt(this.style.left, 10);
+  offsetY = parseInt(this.style.top, 10);
+
+  if (isNaN(offsetX) || isNaN(offsetY)) {
+    offsetX = 0;
+    offsetY = 0;
+  }
+
+  console.log('X: ' + offsetX);
+  console.log('Y: ' + offsetY);
+  inner.addEventListener('mousemove', pan);
+}
+
+function pan(event) {
+  console.log('pan');
+  var x = (event.clientX - initialX + offsetX);
+  var y = (event.clientY - initialY + offsetY);
+  if (x <= 0 && x >= (viewRect.width - cols)) {
+    this.style.left = x + 'px';
+  }
+  if (y <= 0 && y >= (viewRect.height - rows)) {
+    this.style.top = y + 'px';
+  }
+}
+
+function endPan() {
+  console.log('endPan');
+  inner.removeEventListener('mousemove', pan);
 }
