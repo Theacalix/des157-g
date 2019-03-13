870e9d7ab1790119ebd7f42c81bbddd97dd8485d
Theacalix <Theacalix@users.noreply.github.com>
2019-03-13 01:22:03 -0700

    added display mode, split js into module files to run separate files in display and edit modes

--- prototype1/script.js
+++ prototype1/script.js
@@ -1,86 +1,45 @@
-// import { Droppable }  from 'https://cdn.jsdelivr.net/npm/@shopify/draggable@1.0.0-beta.8/lib/draggable.bundle.min.js';
 'use strict';
 console.log('reading js');
-//
-// const droppable = new Droppable(document.querySelectorAll('section .inner'), {
-//   draggable: '.tile',
-//   dropzone: '.box',
-//   mirror: {
-//     constrainDimensions: true,
-//   }
-// });
-
-var acc = document.querySelectorAll('.accordion');
-var but = document.querySelectorAll('.mode button');
-var inner = document.querySelector('.inner');
-var view = document.querySelector('.view');
-var boxRect = document.querySelector('.box').getBoundingClientRect();
-var cols = inner.childElementCount * boxRect.width;
-var rows = document.querySelector('.row').childElementCount * boxRect.height;
-var initialX, initialY;
-var offsetX = 0,
-  offsetY = 0;
-var dragged, tiles = [],
+import * as m from '/modulefunc.js';
+const acc = document.querySelectorAll('.accordion');
+const but = document.querySelectorAll('.mode button');
+const inner = document.querySelector('.inner');
+// var initialX, initialY;
+// var offsetX = 0,
+//   offsetY = 0;
+var dragged,
   curTile = '',
   storeTile;
 var rotateAmt = 0;
-var storage = window.localStorage;
+const storage = window.localStorage;
 
-console.log('cols: ' + cols);
-console.log('rows: ' + rows);
-
-window.addEventListener('load', getLocal);
+window.addEventListener('load', function() {
+  m.getLocal();
+  console.log(m.tiles);
+  clickTiles();
+});
 document.addEventListener('dragstart', startDrag);
 document.addEventListener('dragover', overDrag);
 document.addEventListener('dragenter', enterDrag);
 document.addEventListener('dragleave', leaveDrag);
 document.addEventListener('drop', drop);
 document.addEventListener('click', delesectTile);
-
+//Reset and Display Buttons
 document.querySelector('#reset').addEventListener('click', function() {
   console.log('clearing storage');
   storage.clear();
   location.reload();
 });
 
-function getLocal(event) {
-  console.log('page loaded');
-  var keys = Object.keys(storage),
-    key, box, items, tile;
+document.querySelector('#display').addEventListener('click', function() {
+  window.open('display.html', '_blank');
+});
 
-  for (var i = 0; key = keys[i]; i++) {
-    box = document.querySelector('#' + key);
-    items = storage.getItem(key);
-    items = items.split(',');
-    console.log(key);
-    console.log(items);
-    for (var j = 0; j < items.length; j++) {
-      if (items[j] != '') {
-        var item = items[j].split(';');
-        console.log(item);
-        tile = document.querySelector('#' + item[0]);
-        tile = getTile(tile);
-        tile.style.top = (-100 * j) + 'px';
-        if (rotateAmt = item[1]) {
-          curTile = tile;
-          rotateAmt--;
-          rotate(event);
-          box.appendChild(curTile);
-        } else {
-          box.appendChild(tile);
-        }
-      }
-    }
-    rotateAmt = 0;
-    curTile = '';
-  }
-}
 //ACCORDION MENU
 for (var i = 0; i < acc.length; i++) {
   acc[i].addEventListener('click', function() {
     var icon = this.firstChild;
     var panel = this.nextElementSibling;
-    // console.log(icon);
     if (panel.style.display === 'flex') {
       panel.style.display = 'none';
       icon.className = 'fas fa-caret-right';
@@ -89,7 +48,6 @@ for (var i = 0; i < acc.length; i++) {
       panel.style.display = 'flex';
       icon.className = 'fas fa-caret-down';
       this.style.border = 'none';
-      // console.log(icon);
     }
   })
 }
@@ -99,8 +57,8 @@ but[0].addEventListener('click', function() { //mouse -drag
     this.id = 'active';
     but[1].id = '';
     //remove pan event
-    inner.removeEventListener('mousedown', startPan);
-    inner.removeEventListener('mouseup', endPan);
+    inner.removeEventListener('mousedown', m.startPan);
+    inner.removeEventListener('mouseup', m.endPan);
     //add drag events
     document.addEventListener('dragstart', startDrag);
     document.addEventListener('dragover', overDrag);
@@ -108,9 +66,7 @@ but[0].addEventListener('click', function() { //mouse -drag
     document.addEventListener('dragleave', leaveDrag);
     document.addEventListener('drop', drop);
     document.addEventListener('click', delesectTile);
-    for (var i = 0; i < tiles.length; i++) {
-      tiles[i].addEventListener('click', selectTile);
-    }
+    clickTiles();
   }
 });
 
@@ -126,22 +82,28 @@ but[1].addEventListener('click', function() { //pan
     document.removeEventListener('dragleave', leaveDrag);
     document.removeEventListener('drop', drop);
     document.removeEventListener('click', delesectTile);
-    for (var i = 0; i < tiles.length; i++) {
-      tiles[i].removeEventListener('click', selectTile);
+    for (var i = 0; i < m.length; i++) {
+      m.tiles[i].removeEventListener('click', selectTile);
     }
     //add pan event
-    inner.addEventListener('mousedown', startPan);
-    inner.addEventListener('mouseup', endPan);
+    inner.addEventListener('mousedown', m.startPan);
+    inner.addEventListener('mouseup', m.endPan);
   }
 });
+
+function clickTiles() {
+  for (var i = 0; i < m.tiles.length; i++) {
+    m.tiles[i].addEventListener('click', selectTile);
+  }
+}
 //CLICK MODE
 //drag and drop
 function startDrag(event) {
   console.log('startdrag');
   dragged = event.target;
   if (dragged.parentNode.className == 'box') {
-    var index = Array.from(dragged.parentNode.children).indexOf(dragged) - 2; //for selected
-    storeTile = getItems(dragged)[index];
+    var index = Array.from(dragged.parentNode.children).indexOf(dragged) - 2; //for selected icons
+    storeTile = m.getItems(dragged)[index];
     console.log(storeTile);
   } else {
     storeTile = dragged.id;
@@ -170,31 +132,30 @@ function leaveDrag(event) {
 function drop(event) {
   console.log('drop');
   event.preventDefault();
-  // console.log(event.target);
-  // console.log(event.target.nodeName);
   console.log(storeTile);
   var tile;
   if (event.target.className == 'box' || event.target.nodeName == 'IMG') { //we are going to drop sucessfully
-    if (dragged.parentNode.className == 'box') {
+    if (dragged.parentNode.className == 'box') { //we can delete tile
       curTile = dragged;
+      console.log('node to delete/clone ' + curTile);
+      console.log('parent: ' + curTile.parentNode);
       deleteTile();
     }
-    if (event.target.className == 'box') {
+    if (event.target.className == 'box') { //dragging into empty square
       event.target.style.border = '';
-      tile = getTile(dragged);
+      tile = m.getTile(dragged);
+      tile.addEventListener('click', selectTile);
       event.target.appendChild(tile);
       storage.setItem(event.target.id, storeTile);
       console.log(event.target.id + ',' + storeTile);
-    } else if (event.target.nodeName == 'IMG') {
+    } else if (event.target.nodeName == 'IMG') { //dragging on top of image
       var targetType = event.target.className;
       var dropType = dragged.className;
       var parent = event.target.parentNode;
       var count = parent.childElementCount;
-      // console.log('target: ' + targetType);
-      // console.log('item: ' + dropType);
-      tile = getTile(dragged);
-
-      if (dropType == 'replace' && targetType == 'replace') {
+      tile = m.getTile(dragged);
+      tile.addEventListener('click', selectTile);
+      if (dropType == 'replace' && targetType == 'replace') { //replace tile
         parent.appendChild(tile);
         parent.removeChild(event.target);
         storage.setItem(parent.id, storeTile);
@@ -203,7 +164,7 @@ function drop(event) {
         var store = '';
         var items = storage.getItem(parent.id);
         items = items.split(',');
-        for (var i = 0; i < parent.children.length; i++) {
+        for (var i = 0; i < parent.children.length; i++) { //check if replace tile exists and replace it
           if (parent.children[i].className == 'replace') {
             tile.style.top = parent.children[i].style.top;
             parent.replaceChild(tile, parent.children[i]);
@@ -221,14 +182,13 @@ function drop(event) {
             }
           }
         }
-        if (noReplace) {
+        if (noReplace) { //if not append tile
           tile.style.top = (-100 * count) + 'px';
           parent.appendChild(tile);
           store += ',' + storeTile;
         }
-        // console.log(store);
         storage.setItem(parent.id, store);
-      } else { //dropType = layer
+      } else { //dropType = layer we don't care whats already there
         tile.style.top = (-100 * count) + 'px';
         parent.appendChild(tile);
         // console.log(storeTile);
@@ -239,22 +199,7 @@ function drop(event) {
   }
 }
 
-function getTile(toCopy) {
-  var tile = toCopy.cloneNode();
-  tile.style.top = 0;
-  tile.style.left = 0;
-  tile.setAttribute('draggable', false);
-  tiles.push(tile); //used to add event listeners later
-  tile.addEventListener('click', selectTile);
-  return tile;
-}
-
-function getItems(tile) {
-  var items = storage.getItem(tile.parentNode.id);
-  items = items.split(',');
-  return items;
-}
-//select tiles
+//select tile interactions
 function selectTile() {
   if (curTile !== event.target) {
     delesectTile();
@@ -262,13 +207,11 @@ function selectTile() {
     parent = event.target.parentNode;
     curTile.setAttribute('draggable', true);
     var index = Array.from(curTile.parentNode.children).indexOf(curTile);
-    var item = getItems(curTile)[index].split(';');
-    if (rotateAmt = item[1]) {
-      // console.log(rotateAmt);
-    } else {
+    var item = m.getItems(curTile)[index].split(';');
+    if (rotateAmt = item[1]) {} else {
       rotateAmt = 0;
     }
-    //delete
+    //add delete icon
     var xIcon = document.createElement('i');
     xIcon.className = 'fas fa-times';
     xIcon.style.top = (parent.offsetTop) + 'px';
@@ -276,13 +219,16 @@ function selectTile() {
 
     parent.insertAdjacentElement('afterbegin', xIcon);
     xIcon.addEventListener('click', deleteTile);
-    //rotate
+    //add rotate icon
     var rIcon = document.createElement('i');
     rIcon.className = 'fas fa-undo fa-rotate-270 fa-sm';
     rIcon.style.top = (parent.offsetTop + xIcon.offsetHeight) + 'px';
     rIcon.style.left = (parent.offsetLeft + 100) + 'px';
     parent.insertAdjacentElement('afterbegin', rIcon);
-    rIcon.addEventListener('click', rotate);
+    rIcon.addEventListener('click', function(event) {
+      rotateAmt = m.rotate(rotateAmt, curTile);
+      event.stopPropagation();
+    });
   }
   event.stopPropagation(); //prevent calling cancel event
 }
@@ -296,13 +242,12 @@ function delesectTile() {
     //save rotate
     var index = Array.from(curTile.parentNode.children).indexOf(curTile);
     var store = '';
-    var items = getItems(curTile);
+    var items = m.getItems(curTile);
     items.splice(index, 1, curTile.id + ';' + rotateAmt); //replace with rotate
     store = items[0];
     for (var i = 1; i < items.length; i++) {
       store += ',' + items[i];
     }
-    // console.log(store);
     storage.setItem(parent.id, store);
 
     curTile = '';
@@ -310,77 +255,31 @@ function delesectTile() {
   }
 }
 
-function rotate(event) {
-  console.log('rotate');
-  rotateAmt++;
-  if (rotateAmt > 3) {
-    rotateAmt = 0;
-  }
-  // console.log(curTile);
-  // console.log(rotateAmt * 90);
-  curTile.style.transform = 'rotate(' + (rotateAmt * 90) + 'deg)';
-  event.stopPropagation();
-}
-
-function deleteTile(event) {
+function deleteTile() {
   var parent = curTile.parentNode;
   parent.removeChild(parent.children[0]); //remove rotate
   parent.removeChild(parent.children[0]); //remove delete
   var index = Array.from(parent.children).indexOf(curTile);
-  // console.log(index);
+  console.log('node to delete/clone ' + curTile);
+  console.log('parent: ' + curTile.parentNode);
+  var items = m.getItems(curTile);
   parent.removeChild(curTile);
   rotateAmt = 0;
   curTile = '';
-  //NEED TO UPDATE localStorage
-  if (parent.children.length != 0) {
+
+  if (parent.children.length != 0) { //if still children update position and storage
     for (var i = 0; i < parent.children.length; i++) {
       parent.children[i].style.top = (-100 * i) + 'px';
     } //fix offset
     var store = '';
-    var items = getItems(curTile);
     items.splice(index, 1); //remove item to delete
     store = items[0];
     for (var i = 1; i < items.length; i++) {
       store += ',' + items[i];
     }
-    // console.log(store);
+    console.log(store);
     storage.setItem(parent.id, store);
-  } else { //no children;
+  } else { //no children remove storage
     storage.removeItem(parent.id);
   }
-}
-
-//PAN MODE
-function startPan(event) {
-  console.log('startPan');
-  initialX = event.clientX;
-  initialY = event.clientY;
-  offsetX = parseInt(this.style.left, 10);
-  offsetY = parseInt(this.style.top, 10);
-
-  if (isNaN(offsetX) || isNaN(offsetY)) {
-    offsetX = 0;
-    offsetY = 0;
-  }
-
-  // console.log('X: ' + offsetX);
-  // console.log('Y: ' + offsetY);
-  inner.addEventListener('mousemove', pan);
-}
-
-function pan(event) {
-  console.log('pan');
-  var x = (event.clientX - initialX + offsetX);
-  var y = (event.clientY - initialY + offsetY);
-  if (x <= 0 && x >= (view.clientWidth - cols)) {
-    this.style.left = x + 'px';
-  }
-  if (y <= 0 && y >= (view.clientHeight - rows)) {
-    this.style.top = y + 'px';
-  }
-}
-
-function endPan() {
-  console.log('endPan');
-  inner.removeEventListener('mousemove', pan);
 }
---
+++ prototype1/scriptdisplay.js
@@ -0,0 +1,22 @@
+import {
+  getLocal,
+  startPan,
+  endPan
+} from '/modulefunc.js';
+
+// var storage = window.localStorage;
+// var curTile,
+//   rotateAmt,
+//   initialX,
+//   initialY,
+//   offsetX = 0,
+//   offsetY = 0;
+const inner = document.querySelector('.inner');
+// var view = document.querySelector('.view');
+// var boxRect = document.querySelector('.box').getBoundingClientRect();
+// var cols = inner.childElementCount * boxRect.width;
+// var rows = document.querySelector('.row').childElementCount * boxRect.height;
+
+window.addEventListener('load', getLocal);
+inner.addEventListener('mousedown', startPan);
+inner.addEventListener('mouseup', endPan);
